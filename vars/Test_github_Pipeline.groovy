33a9c5f904a188495444f818a542ef35d7ae7bb8
edy <edy@edydeMacBook-Air.local>
2025-03-05 11:19:55 +0800

    add test pipeline

---
+++ vars/Test_github_Pipeline.groovy
@@ -0,0 +1,16 @@
+#!groovy
+def call(body) {
+    pipeline {
+        agent any
+        stages {
+            stage('Handle Webhook') {
+                steps {
+                    script {
+                        // 通过 params.payload 访问变量
+                        echo "GitHub Event Payload: $all"
+                    }
+                }
+            }
+        }
+    }
+}
