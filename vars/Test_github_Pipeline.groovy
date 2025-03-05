8c32106f5f1cdb620db77610147ec0768d08575f
silent.mo <1916393131@qq.com>
2025-03-05 19:30:59 +0800

    update test github echo

--- vars/Test_github_Pipeline.groovy
+++ vars/Test_github_Pipeline.groovy
@@ -7,12 +7,12 @@ def call(body) {
             }
         }
         stages  {
-            // stage("Docker PS") {
-            //     steps {
-            //         // sh 'docker ps'
-            //         sh 'sleep 1800'
-            //     }
-            // }
+            stage("get_evn") {
+                steps {
+                    // sh 'docker ps'
+                    echo "${env.all}"
+                }
+            }
             stage("Build for AMD64 platform") {
               agent {
                 kubernetes {
@@ -21,8 +21,9 @@ def call(body) {
               }
               steps {
                 container('kaniko') {
-                    sh 'sleep 18000'
-                    sh '/kaniko/executor --context `pwd` --dockerfile `pwd`/Dockerfile --destination 617482875210.dkr.ecr.us-east-1.amazonaws.com/java-demo:202310-02-amd64'
+                    echo 'Building the Docker image'
+                    // sh 'sleep 18000'
+                    // sh '/kaniko/executor --context `pwd` --dockerfile `pwd`/Dockerfile --destination 617482875210.dkr.ecr.us-east-1.amazonaws.com/java-demo:202310-02-amd64'
                 }
               }
             }
