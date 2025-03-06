db541aa9599a040fd612b6162bb978bc2a10e4e5
silent.mo <1916393131@qq.com>
2025-03-06 20:08:02 +0800

    update WebIdentityTokenCredentialsProvider

--- src/org/aws/ECR.groovy
+++ src/org/aws/ECR.groovy
@@ -1,68 +1,72 @@
 #!groovy
 package org.aws
-// aws sdk v2
-import software.amazon.awssdk.auth.credentials.AwsSessionCredentials
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
-import software.amazon.awssdk.regions.Region
-import software.amazon.awssdk.services.ecr.EcrClient
-import software.amazon.awssdk.services.ecr.EcrClientBuilder
-import software.amazon.awssdk.services.ecr.model.CreateRepositoryRequest
-import software.amazon.awssdk.services.ecr.model.ResourceAlreadyExistsException
-import software.amazon.awssdk.services.ecr.model.GetAuthorizationTokenResponse
-import software.amazon.awssdk.services.sts.StsClient
-import software.amazon.awssdk.services.sts.StsClientBuilder
-import software.amazon.awssdk.services.sts.model.AssumeRoleRequest
-import java.util.Base64
-import java.util.Arrays
+// import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.auth.BasicSessionCredentials
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.services.ecr.AmazonECRClient
+import com.amazonaws.services.ecr.AmazonECRClientBuilder
+import com.amazonaws.services.ecr.model.CreateRepositoryRequest
+import com.amazonaws.services.ecr.model.CreateRepositoryResult
+import com.amazonaws.services.ecr.model.RepositoryAlreadyExistsException
+import com.amazonaws.services.ecr.model.GetAuthorizationTokenRequest
+import com.amazonaws.services.ecr.model.GetAuthorizationTokenResult
+import com.amazonaws.services.securitytoken.model.AssumeRoleRequest
+import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder
+import com.amazonaws.services.securitytoken.AWSSecurityTokenService
+
 
 def createRepository(String region, String repoName, String roleArn) {    
-    EcrClient ecrClient
+    AmazonECRClient ecrClient
+    String sa_roleArn = System.getenv("AWS_ROLE_ARN")
+    String tokenFile = System.getenv("AWS_WEB_IDENTITY_TOKEN_FILE")
+    if (!sa_roleArn || !tokenFile) {
+        throw new Exception("IRSA 环境变量未正确配置: AWS_ROLE_ARN 或 AWS_WEB_IDENTITY_TOKEN_FILE 缺失")
+    }
+    println "***INFO: Using IRSA credentials"
+    // 显式配置 WebIdentityTokenCredentialsProvider
+    WebIdentityTokenCredentialsProvider credentialsProvider = new WebIdentityTokenCredentialsProvider(
+        sa_roleArn,
+        "jenkins-session",  // roleSessionName 可自定义
+        tokenFile
+    )
     if (roleArn) {
-        // STS assumeRole
+        // sts assumeRole
         println "***INFO: Assume Role ${roleArn}"
-        AssumeRoleRequest assumeRole = AssumeRoleRequest.builder()
-                .roleArn(roleArn)
-                .roleSessionName("jenkins-slave-login-ecr")
-                .build()
+        AssumeRoleRequest assumeRole = new AssumeRoleRequest().withRoleArn(roleArn).withRoleSessionName("jenkins-slave-login-ecr");
+        
+        AWSSecurityTokenService sts = AWSSecurityTokenServiceClientBuilder.standard().withRegion(region).withCredentials(credentialsProvider).build();
+        credentials = sts.assumeRole(assumeRole).getCredentials();
 
-        StsClient sts = StsClientBuilder.standard()
-                .region(Region.of(region))
-                .build()
-        def credentials = sts.assumeRole(assumeRole).credentials()
+        BasicSessionCredentials sessionCredentials = new BasicSessionCredentials(
+                credentials.getAccessKeyId(),
+                credentials.getSecretAccessKey(),
+                credentials.getSessionToken());
 
-        AwsSessionCredentials sessionCredentials = AwsSessionCredentials.create(
-                credentials.accessKeyId(),
-                credentials.secretAccessKey(),
-                credentials.sessionToken())
-
-        ecrClient = EcrClientBuilder.standard()
-                .credentialsProvider(StaticCredentialsProvider.create(sessionCredentials))
-                .region(Region.of(region))
+        ecrClient = AmazonECRClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(sessionCredentials))
+                .withRegion(region)
                 .build()
     } else {
-        println "***INFO: Using IRSA credentials"
-        ecrClient = EcrClientBuilder.standard()
-                .region(Region.of(region))
-                .build()
+        ecrClient = AmazonECRClientBuilder.standard()
+            .withRegion(region)
+            .withCredentials(credentialsProvider)  // 使用 WebIdentityTokenCredentialsProvider
+            .build()
     }
     println "***INFO: AWS ECR Client: ${ecrClient}"
-
-    // 获取授权令牌
+    GetAuthorizationTokenRequest request = new GetAuthorizationTokenRequest()
     println "***INFO: AWS ECR Getting Authorization Token."
-    GetAuthorizationTokenResponse response = ecrClient.getAuthorizationToken()
-    String token = response.authorizationData().get(0).authorizationToken()
-    String[] ecrCreds = new String(Base64.getDecoder().decode(token), 'UTF-8').split(':')
-    def result = Arrays.asList(ecrCreds)
-    println "***INFO: AWS ECR Authorization Token: ${result}"
-
-    // 创建仓库
-    CreateRepositoryRequest createRequest = CreateRepositoryRequest.builder()
-            .name(repoName)
-            .build()
+    GetAuthorizationTokenResult response = ecrClient.getAuthorizationToken(request)
+    //println response.getAuthorizationData()
+    token = response.getAuthorizationData().get(0).getAuthorizationToken()
+    String[] ecrCreds = new String(token.decodeBase64(), 'UTF-8').split(':')
+    result = java.util.Arrays.asList(ecrCreds)
+    
+    CreateRepositoryRequest createRequest = new CreateRepositoryRequest().withRepositoryName(repoName)
+    //println (createRequest)
     try {
         println "***INFO: AWS ECR Creating Repository ${repoName}."
-        ecrClient.createRepository(createRequest)
-    } catch (ResourceAlreadyExistsException e) {
+        CreateRepositoryResult createResult = ecrClient.createRepository(createRequest)
+    } catch (RepositoryAlreadyExistsException e) {
         println "***INFO: AWS ECR Repository ${repoName} already exists."
     }
 
