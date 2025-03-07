80cf19466eccc57daf3a4e59b77cf73d5fc6bda4
silent.mo <1916393131@qq.com>
2025-03-07 15:44:52 +0800

    udpate echo

--- src/org/Tools.groovy
+++ src/org/Tools.groovy
@@ -16,7 +16,6 @@ def checkoutSource( String repoUrl, String branch, String gitCredentialsId ){
             cp $SSH_KEY_FILE ~/.ssh/id_rsa
             chmod 600 ~/.ssh/id_rsa
             ssh-keyscan github.com >> ~/.ssh/known_hosts
-            echo $SSH_KEY_FILE
         '''
 
         checkout([
