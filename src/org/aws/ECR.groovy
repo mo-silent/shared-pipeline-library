9bc11fac18fa5c26a3f1b9146516fc5d44d52bbe
silent.mo <1916393131@qq.com>
2025-03-09 22:49:54 +0800

    update ecr create

--- src/org/aws/ECR.groovy
+++ src/org/aws/ECR.groovy
@@ -4,20 +4,25 @@ package org.aws
 def createRepository(String region, String repoName, String roleArn) {
     if (roleArn) {
         sh "aws sts assume-role --role-arn ${roleArn} --role-session-name jenkins-slave-login-ecr --region ${region} > creds.json"
-    } else {
-        sh "aws sts get-session-token --duration-seconds 900 --output json > creds.json"
-    }
-    sh "cat creds.json"
-    def creds = readJSON file: 'creds.json'
-    withEnv(["AWS_ACCESS_KEY_ID=${creds.Credentials.AccessKeyId}",
-                "AWS_SECRET_ACCESS_KEY=${creds.Credentials.SecretAccessKey}",
-                "AWS_SESSION_TOKEN=${creds.Credentials.SessionToken}"]) {
-        sh "aws ecr get-login-password --region ${region} > token.txt"
-        def repoExists = sh(script: "aws ecr describe-repositories --repository-names ${repoName} --region ${region}", returnStatus: true) == 0
-        if (!repoExists) {
-            sh "aws ecr create-repository --repository-name ${repoName} --region ${region}"
+        def creds = readJSON file: 'creds.json'
+        withEnv(["AWS_ACCESS_KEY_ID=${creds.Credentials.AccessKeyId}",
+                    "AWS_SECRET_ACCESS_KEY=${creds.Credentials.SecretAccessKey}",
+                    "AWS_SESSION_TOKEN=${creds.Credentials.SessionToken}"]) {
+            createNewRepository(region, repoName)
         }
+    } else {
+        createNewRepository(region, repoName)
     }
+    
     def token = readFile('token.txt').trim()
     return [token] // 简化返回值
+}
+
+@NonCPS
+def createNewRepository(String region, String repoName){
+    sh "aws ecr get-login-password --region ${region} > token.txt"
+    def repoExists = sh(script: "aws ecr describe-repositories --repository-names ${repoName} --region ${region}", returnStatus: true) == 0
+    if (!repoExists) {
+        sh "aws ecr create-repository --repository-name ${repoName} --region ${region}"
+    }
 }
