23b1346db25047a771c6860838227f50e12930d9
silent.mo <1916393131@qq.com>
2025-03-10 20:19:44 +0800

    update dir

--- src/org/docker/Utils.groovy
+++ src/org/docker/Utils.groovy
@@ -4,7 +4,7 @@ package org.docker
 def createWebDockerfile(String path, Map METADATA){
     sh """
         cd Dockerfile/plaud-web/
-        mkdir -p /data/${path}
+        mkdir -p ../data/${path}
         // 读取package.json中的name字段作为publicPath
         publicPath=\$(cat ${METADATA.repo_dir}/${path}/package.json | grep "name" | cut -d'"' -f4)
         echo "\${publicPath}"
@@ -13,10 +13,10 @@ def createWebDockerfile(String path, Map METADATA){
         dockerfile_content=\$(if [ "${path}" = "editor" ]; then cat editor_Dockerfile; else cat Dockerfile; fi)
         
         # 使用 sed 替换\$path为传入的 path 参数
-        # echo "\${dockerfile_content}" | sed -e "s/\\\$env/${METADATA.packegePUBLISH_PATH}/g" -e "s/\\\$path/\${publicPath}/g" > ${path}/Dockerfile.tmp
-        sed -e "s/\\\$path/\${publicPath}/g" > ${path}/Dockerfile.tmp
+        # echo "\${dockerfile_content}" | sed -e "s/\\\$env/${METADATA.packegePUBLISH_PATH}/g" -e "s/\\\$path/\${publicPath}/g" > ../data/${path}/Dockerfile.tmp
+        sed -e "s/\\\$path/\${publicPath}/g" > ../data/${path}/Dockerfile.tmp
         
         # 覆盖原始Dockerfile
-        mv /data/${path}/Dockerfile.tmp /data/${path}/Dockerfile
+        mv ../data/${path}/Dockerfile.tmp ../data/${path}/Dockerfile
     """
 }
